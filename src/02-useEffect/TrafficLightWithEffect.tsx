a5e4f8621d748ff0f1473ce6082960179f866084
dtovar <programerbyte@gmail.com>
2025-09-04 07:31:52 -0400

    Agregando use effect

--- src/02-useEffect/TrafficLightWithEffect.tsx
+++ src/02-useEffect/TrafficLightWithEffect.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const colors = {
     red: 'bg-red-500 animate-pulse',
@@ -11,10 +11,30 @@ type TrafficLightColor = keyof typeof colors;
 export const TrafficLightWithEffect = () => {
 
     const [light, setLight] = useState<TrafficLightColor>('red');
+    const [countdown, setCountdown] = useState(5);
+
+    useEffect(() => {
+       
+        if(countdown === 0) return;
+
+
+        const intervalId = setInterval(() =>{
+            setCountdown((prev) => prev - 1);
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId);
+        }
+
+     }, [countdown]);
+        
 
     return (
         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
         <div className="flex flex-col items-center space-y-8">
+
+            <h1 className="text-white text-3xl font thin">Semáforo con useEffect</h1>
+            <h2 className="text-white text-3xl font thin">Countdown {countdown}</h2>
 
 
             <div className={`w-32 h-32 ${ light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
