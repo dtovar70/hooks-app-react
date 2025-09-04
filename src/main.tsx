4c51b06a506d152cdc50ea798c772f6b5493e57c
dtovar <programerbyte@gmail.com>
2025-09-03 22:05:43 -0400

    Add

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TrafficLightWithEffect />
   </StrictMode>,
 )
