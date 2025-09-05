0ac479a1baaff289368fafe27b3ec952293c2b39
dtovar <programerbyte@gmail.com>
2025-09-04 23:12:00 -0400

    Agregando cambios nuevos de semaforo

--- src/02-useEffect/TrafficLightWithEffect.tsx
+++ src/02-useEffect/TrafficLightWithEffect.tsx
@@ -14,11 +14,10 @@ export const TrafficLightWithEffect = () => {
     const [countdown, setCountdown] = useState(5);
 
     useEffect(() => {
-       
+
         if(countdown === 0) return;
 
-
-        const intervalId = setInterval(() =>{
+        const intervalId = setInterval(() => {
             setCountdown((prev) => prev - 1);
         }, 1000)
 
@@ -26,15 +25,48 @@ export const TrafficLightWithEffect = () => {
             clearInterval(intervalId);
         }
 
-     }, [countdown]);
-        
+     }, [countdown, light]);
 
+     //change light color effect
+
+     useEffect(() => {
+
+        if( countdown > 0) return;
+
+        setCountdown(5);
+
+         if(countdown === 0){
+            
+            if(light === 'red') {
+                setLight('green');
+                return;
+            }
+
+            if(light === 'yellow') {
+                setLight('red');
+                return;
+            }
+
+            if(light === 'green') {
+                setLight('yellow');
+                return;
+            }
+        } 
+
+     }, [countdown, light]);
+
+     
     return (
         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
         <div className="flex flex-col items-center space-y-8">
 
             <h1 className="text-white text-3xl font thin">Semáforo con useEffect</h1>
             <h2 className="text-white text-3xl font thin">Countdown {countdown}</h2>
+
+            <div className="w-64 bg-gray-700 rounded-full h-2">
+                <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
+                 style={{ width: `${(countdown/5) * 100 }%` }}></div>
+            </div>
 
 
             <div className={`w-32 h-32 ${ light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
