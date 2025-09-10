24c88d2bb9cdae2a9b3b7aa23f34353ffe8e8d56
jtovar <jtovar@ks2.tech>
2025-09-10 15:27:34 -0400

    Agregado cambios

--- src/05-useReducer/TaskApp.tsx
+++ src/05-useReducer/TaskApp.tsx
@@ -6,7 +6,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { log } from 'console';
 
 interface Todo {
   id: number;
--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -16,8 +16,53 @@ export type TaskSAction =
 | { type: 'TOGGLE_TODO', payload: number }
 | { type: 'DELETE_TODO', payload: number }
 
-const tasksReducer = (state, action):TaskState => {
-  return state;
+export const tasksReducer = (
+  state: TaskState, 
+  action: TaskSAction
+):TaskState => {
+
+  switch(action.type){
+    case 'ADD_TODO':
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false
+      }
+    
+    return {
+      ...state,
+      todos: [...state.todos, newTodo],
+      length: state.length + 1,
+      pending: state.pending + 1
+    }
+
+  case 'TOGGLE_TODO':
+    const updateTodos = state.todos.map( todo => {
+        if(todo.id === action.payload){
+            return {...todo, completed: !todo.completed}
+        }
+      return todo;
+    });
+
+    return {
+        ...state,
+        todos: updateTodos,
+        completed: updateTodos.filter(todo => todo.completed).length,
+        pending: updateTodos.filter(todo => !todo.completed).length
+    }
+
+    case 'DELETE_TODO':
+      const currentTodos = state.todos.filter(todo => todo.id !== action.payload);
+      return {
+        ...state,
+        todos: currentTodos,
+        length: currentTodos.length,
+        pending: currentTodos.filter(todo => !todo.completed).length,
+        completed: currentTodos.filter(todo => todo.completed).length
+      }
+
+    default:
+     return state;
+  }
 }
 
-export default tasksReducer
