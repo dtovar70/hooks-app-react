8ea37f550376bb2aeee4d6674defdb42bc3db43e
jtovar <jtovar@ks2.tech>
2025-09-29 14:39:39 -0400

    Changed

--- src/08-use-suspense/ClientInformation.tsx
+++ src/08-use-suspense/ClientInformation.tsx
@@ -1,7 +1,13 @@
-import React, { useEffect } from 'react'
-import { getUserAction } from './api/get-user.action'
+import React, { use, useEffect, type Usable } from 'react'
+import { type User } from './api/get-user.action'
 
-export const ClientInformation = ({ id }: { id:number}) => {
+interface Props {
+  getUser: Usable<User>
+}
+
+export const ClientInformation = ({ getUser }:Props) => {
+
+  const user = use(getUser)
 
 /* useEffect(() => {
     getUserAction(id).then(console.log)
@@ -9,10 +15,10 @@ export const ClientInformation = ({ id }: { id:number}) => {
 
   return (
     <div className='bg-gradient flex flex-col gap-4'>
-        <h2 className='text-4xl font-thin text-white'>Fernando - #123</h2>
+        <h2 className='text-4xl font-thin text-white'>{ user.name} - #{user.id}</h2>
 
-        <p className='text-white text-2xl'>Ottawa, Canada</p>
-        <p className='text-white text-xl'>Un role del usuario</p>
+        <p className='text-white text-2xl'>{ user.location}</p>
+        <p className='text-white text-xl'>{ user.role}</p>
 
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster, toast } from 'sonner'
 import './index.css'
 import ClientInformation from './08-use-suspense/ClientInformation.tsx'
+import { getUserAction } from './08-use-suspense/api/get-user.action.ts'
 /* import { InstagromApp } from './07-useOptimistic/InstagromApp.tsx' */
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Toaster richColors/>
-    <ClientInformation  id={100}/>
+    <Suspense fallback={
+      <div className='bg-gradient flex flex-col'>
+        <h1 className='text-2xl font-thin'>Cargando</h1>
+      </div>
+      }
+    >
+    <ClientInformation  getUser={ getUserAction(100)}/>
+    </Suspense>
   </StrictMode>,
 )
