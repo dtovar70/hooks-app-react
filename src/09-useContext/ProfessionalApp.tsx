90cc91ac15820df1beb6d50361e1a9163eaccf2a
jtovar <jtovar@ks2.tech>
2025-09-29 16:58:02 -0400

    changed

---
+++ src/09-useContext/ProfessionalApp.tsx
@@ -0,0 +1,12 @@
+import { Route } from "lucide-react"
+import { RouterProvider } from "react-router"
+import { appRouter } from "./router/app.router"
+
+
+export const ProfessionalApp = () => {
+  return (
+    <div className="bg-gradient">
+      < RouterProvider router={appRouter} />
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
-import { StrictMode, Suspense } from 'react'
+import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster, toast } from 'sonner'
 import './index.css'
-import ClientInformation from './08-use-suspense/ClientInformation.tsx'
-import { getUserAction } from './08-use-suspense/api/get-user.action.ts'
-/* import { InstagromApp } from './07-useOptimistic/InstagromApp.tsx' */
+import { ProfessionalApp } from './09-useContext/ProfessionalApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={
-      <div className='bg-gradient flex flex-col'>
-        <h1 className='text-2xl font-thin'>Cargando</h1>
-      </div>
-      }
-    >
-    <ClientInformation  getUser={ getUserAction(100)}/>
-    </Suspense>
+    <ProfessionalApp />
   </StrictMode>,
 )
